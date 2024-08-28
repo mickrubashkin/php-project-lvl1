e004f51001ebbe568c08e335f24d450a9e482f79
rubashkin <rubashkin@MacBook-Pro.local>
2024-08-28 17:10:26 +0200

    fix step1

--- src/Cli.php
+++ src/Cli.php
@@ -5,6 +5,6 @@ namespace Braingames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s", $name);
+line('/Welcome to the Brain Game!/');
+$name = prompt('/May I have your name?/');
+line("/Hello, %s!/", $name);
